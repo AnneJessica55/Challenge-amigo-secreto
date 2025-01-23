676e3c1bcb0d37515506b159b1e9358cba9cee99
Anne Jéssica <annejessica55@gmail.com>
2025-01-23 12:38:19 -0300

    Correnção 3

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeAmigos = [];
-let amigoAleatorio = parseInt(Math.random() * listaDeAmigos.length);
-    let amigosSorteados = [];
+let amigoAleatorio;
+let presenteadoAleatorio;
+let amigosSorteados = [];
+let presenteadosSorteados = [];
+let listaDeSorteados = [];
 
 function adicionarAmigo(){
    let amigoAdicionado = document.getElementById('amigo').value; 
    if(amigoAdicionado != "" && listaDeAmigos.includes(amigoAdicionado) == false){
      listaDeAmigos.push(amigoAdicionado);
      
-        escreverNaTela('listaAmigos');  
+        escreverNaTela('listaAmigos', listaDeAmigos);  
     
    }else{
     if(listaDeAmigos.includes(amigoAdicionado) == true){
@@ -19,31 +22,62 @@ function adicionarAmigo(){
         
     }
     limparCampo();
+    amigosSorteados = [];
+    presenteadosSorteados = [];
+}
+
+function aleatorizar (){
+    let aleatorio = parseInt(Math.random() * listaDeAmigos.length - 0.5);
+    return aleatorio;
 }
 
 
-function escreverNaTela(id){
-    let campo = document.getElementById(id);
-    campo.innerHTML = listaDeAmigos.map(item => `<li>${item}</li>`).join("");
+function escreverNaTela(id, lista){
+    let taG = document.createElement('li');
+    for(var i = 0; i < lista.length; i++){
+        
+        taG.textContent = lista[i];
+                
+    }
+    document.getElementById(id).appendChild(taG);
+   
 }
+
 function limparCampo(){
    amigoAdicionado = document.getElementById('amigo');
    document.getElementById('resultado').innerHTML = '';
     amigoAdicionado.value = '';
 }
 function sortearAmigo(){
-    
+    amigoAleatorio = aleatorizar();
+    presenteadoAleatorio = aleatorizar();
+    if(listaDeAmigos.length % 2 == 0){
+        if(amigosSorteados.length != listaDeAmigos.length){
+            while(amigoAleatorio == presenteadoAleatorio){
+                amigoAleatorio = aleatorizar();
+                presenteadoAleatorio = aleatorizar();
+            }
 
-    if(amigosSorteados.length == listaDeAmigos.length){
-        document.getElementById('resultado').innerHTML = "Todos já foram sorteados"
-    } else{
-        while(amigosSorteados.includes(amigoAleatorio) == true){
-            amigoAleatorio = parseInt(Math.random() * listaDeAmigos.length);
+            while(amigosSorteados.includes(amigoAleatorio) == true || presenteadosSorteados.includes(presenteadoAleatorio) == true){
+                amigoAleatorio = aleatorizar();
+                presenteadoAleatorio = aleatorizar();
+            }
+
+            amigosSorteados.push(amigoAleatorio);
+            presenteadosSorteados.push(presenteadoAleatorio);
+            //Criando um array de texto
+            listaDeSorteados.push(`${listaDeAmigos[amigoAleatorio]} ---> ${listaDeAmigos[presenteadoAleatorio]}`);
+
+            escreverNaTela('resultado', listaDeSorteados);
+
+        }else{
+            document.getElementById('resultado').innerHTML = "Todos ja foram Sorteados"
+            listaDeSorteados = [];
+            amigosSorteados = [];
+            presenteadosSorteados = [];
         }
-        amigosSorteados.push(amigoAleatorio);
-        document.getElementById('resultado').innerHTML = listaDeAmigos[amigoAleatorio];
-           
-    }
-       
 
+    } else{
+        document.getElementById('resultado').innerHTML = 'A quantidade de participantes deve ser par.'
+    }
 }
